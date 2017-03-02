ff7b4a6589adafd1e7bf93eda80419ba9c6c0437
钟乘永 <zhongcy@163.com>
2017-03-02 08:33:10 +0800

    代码提交

---
+++ src/main/java/com/kode/designpattern/dutychain/HandlerFactory.java
@@ -0,0 +1,28 @@
+package com.kode.designpattern.dutychain;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by zhongcy on 2017-03-02.
+ */
+public class HandlerFactory {
+    private List<Handler> handlers;
+    public void setSuccessors(List<Handler> handlers){
+        this.handlers = handlers;
+        Iterator<Handler> iterator = handlers.iterator();
+        if(iterator.hasNext()) {
+            Handler handler = iterator.next();
+            while (iterator.hasNext()) {
+                Handler nextHandler = iterator.next();
+                handler.setSuccessor(nextHandler);
+                handler = nextHandler;
+            }
+        }
+    }
+    public void handleEvent(int day){
+        if(handlers!=null){
+            handlers.iterator().next().handle(day);
+        }
+    }
+}
---
+++ src/main/java/com/kode/designpattern/dutychain/Main.java
@@ -0,0 +1,15 @@
+package com.kode.designpattern.dutychain;
+
+import java.util.Arrays;
+
+/**
+ * 构造一个责任链，一直找到合适处理的节点
+ * Created by zhongcy on 2017-03-02.
+ */
+public class Main {
+    public static void main(String[] args) {
+        HandlerFactory handlerFactory =new HandlerFactory();
+        handlerFactory.setSuccessors(Arrays.asList(new Employee(),new Manager(),new Boss()));
+        handlerFactory.handleEvent(1);
+    }
+}
