b95a71a9b220a44c9ac230d37f6e51be3ff0fdf9
钟乘永 <zhongcy@163.com>
2017-03-18 23:08:23 +0800

    数学题

--- src/main/java/com/kode/test/proxy/impl/CgLibProxy.java
+++ src/main/java/com/kode/test/proxy/impl/CgLibProxy.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Method;
  * cglib的动态代理：
  * cglib使用ASM这一轻量但高性能的字节码操作框架来转化字节码，产生新类。
  * 本质上来说，对于需要被代理的类，它只是动态生成一个子类覆盖非final的方法，
- * 同时绑定钩子回调自定义的拦截器，比cglib动态代理快。
+ * 同时绑定钩子回调自定义的拦截器，比jdk动态代理快。
  * Created by zhongcy on 2016/6/3.
  */
 public class CgLibProxy implements MethodInterceptor {
--- src/main/java/com/kode/test/proxy/main/JdkProxy.java
+++
@@ -1,8 +0,0 @@
-package com.kode.test.proxy.main;
-
-/**
- * Created by zhongcy on 2016/6/3.
- */
-public class JdkProxy<T> {
-
-}
