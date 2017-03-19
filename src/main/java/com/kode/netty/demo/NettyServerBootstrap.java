033f5647d1c7b434ad86b2e4a4a079fbddaeba1b
钟乘永 <zhongcy@163.com>
2017-03-19 23:23:30 +0800

    info0319

---
+++ src/main/java/com/kode/netty/demo/NettyServerBootstrap.java
@@ -0,0 +1,63 @@
+package com.kode.netty.demo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServerBootstrap {
+    private int port;
+
+    public NettyServerBootstrap(int port) {
+        this.port = port;
+        bind();
+    }
+
+    private void bind() {
+
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            //步骤：1.注册两个EventLoopGroup 2.创建channel 3.设置连接数 4.设置TCP延迟 5.设置长连接 6.设置Handler
+            bootstrap.group(boss, worker);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_BACKLOG, 1024); //连接数，默认是100
+            bootstrap.option(ChannelOption.TCP_NODELAY, true);  //不延迟，消息立即发送
+            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true); //长连接
+            bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel)
+                        throws Exception {
+                    socketChannel.pipeline().addLast(new NettyServerHandler());
+                }
+            });
+            ChannelFuture f = bootstrap.bind(port).sync();
+            if (f.isSuccess()) {
+                System.out.println("启动Netty服务成功，端口号：" + this.port);
+            }
+            // 关闭连接
+            f.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+            System.out.println("启动Netty服务异常，异常信息：" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        NettyServerBootstrap server = new NettyServerBootstrap(9999);
+
+    }
+
+}
